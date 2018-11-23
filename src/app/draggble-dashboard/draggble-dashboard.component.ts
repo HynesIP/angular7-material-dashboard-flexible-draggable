0835b83c63728d48aa8c89939c36ef3272ce2bba
yuri.araujo <yuri.araujo@e-xyon.com.br>
2018-11-23 18:32:07 -0200

    added resizable poor solution

--- src/app/draggble-dashboard/draggble-dashboard.component.ts
+++ src/app/draggble-dashboard/draggble-dashboard.component.ts
@@ -1,16 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import * as Plotly from 'src/assets/plotly.min.js';
 
 @Component({
   selector: 'app-draggble-dashboard',
   templateUrl: './draggble-dashboard.component.html',
   styleUrls: ['./draggble-dashboard.component.sass'],
 })
-export class DraggbleDashboardComponent {
+export class DraggbleDashboardComponent implements AfterViewInit {
+
+  @ViewChild('plotly_graph') plotly_graph: ElementRef;
+
+
   first = [
     'CARD 1',
     'CARD 2',
-    
+
   ];
 
   second = [
@@ -20,17 +25,43 @@ export class DraggbleDashboardComponent {
 
   third = [
     'CARD 5',
-    'CARD 6'
+    'CARD 6',
+    'CARD 7',
+    'CARD 8'
   ];
+
+  data = [{
+    x: [1, 2, 3],
+    y: [0, 0.5, 1],
+    line: {simplify: false},
+    marker: {
+      color: '#C8A2C8',
+      line: {
+        width: 2.5
+      }
+    }
+  }];
+
+  layout = {
+    title: 'Plotly',
+    font: { size: 18 }
+  };
 
   drop(event: CdkDragDrop<string[]>) {
     if (event.previousContainer === event.container) {
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
     } else {
       transferArrayItem(event.previousContainer.data,
-          event.container.data,
-          event.previousIndex,
-          event.currentIndex);
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex);
+      console.log('else');
+      console.log('if');
+      Plotly.newPlot(this.plotly_graph.nativeElement, this.data, this.layout, { responsive: true });
     }
+  }
+
+  ngAfterViewInit() {
+    Plotly.newPlot(this.plotly_graph.nativeElement, this.data, this.layout, { responsive: true });
   }
 }
